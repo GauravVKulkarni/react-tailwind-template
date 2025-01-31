c2042ef71ad2c07bb59f06aba93a55f6c93b537b
GauravVKulkarni <gauravvilaskulkarni9211@gmail.com>
2025-01-31 23:18:08 +0530

    created template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-blue-500">Yup! it's working</h1>
+      <div className="w-40 h-40 bg-red-400">Yup! tailwind's working too!</div>
+    </>
+  )
+}
+
+export default App
